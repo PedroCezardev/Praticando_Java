30302ca914eb5d9deefd1138cd77a823fe5b3483
Pedro Cezar <128489164+PedroCezardev@users.noreply.github.com>
2023-07-26 23:03:30 -0300

    Update Mavenproject1.java

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -1,6 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 //Programa de análise de números ímpar ou par.
 
 package com.mycompany.mavenproject1;
